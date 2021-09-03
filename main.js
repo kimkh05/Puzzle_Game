706710b3cfc62ede4023feda5e3261aa7982979d
김경호 <nestjs01@naver.com>
2021-09-03 19:08:10 +0900

    🖼️Slide Puzzle🖼️

---
+++ main.js
@@ -0,0 +1,6 @@
+if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+  document.documentElement.classList.add("dark")
+  }
+  document.getElementById("toggleTheme").addEventListener("click",() => {
+  document.documentElement.classList.toggle("dark")
+  })
