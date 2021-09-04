1cf3fa60fb6d9fbce85ea374ea49a1919637c018
김경호 <nestjs01@naver.com>
2021-09-05 00:41:53 +0900

    🖼️Slide Puzzle Project🖼️
    making menu html

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
   document.documentElement.classList.add("dark")
   }
-  document.getElementById("toggleTheme").addEventListener("click",() => {
+  document.getElementById("darkTheme").addEventListener("click",() => {
   document.documentElement.classList.toggle("dark")
   })
+  // 다크모드 적용함
